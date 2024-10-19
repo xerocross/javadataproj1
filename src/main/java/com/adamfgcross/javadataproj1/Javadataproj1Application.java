79626e2fb94dfb59d0b6ec392c817e893495497e
Adam <adamfgcross@gmail.com>
2024-10-19 12:21:16 -0400

    first commit

---
+++ src/main/java/com/adamfgcross/javadataproj1/Javadataproj1Application.java
@@ -0,0 +1,13 @@
+package com.adamfgcross.javadataproj1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Javadataproj1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Javadataproj1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/adamfgcross/javadataproj1/Javadataproj1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adamfgcross.javadataproj1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Javadataproj1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
